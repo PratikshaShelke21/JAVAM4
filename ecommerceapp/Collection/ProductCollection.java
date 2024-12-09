e07551ffbf9c5f072287b75bfb33868313d99414
PratikshaShelke21 <shelkepratu2102@gmail.com>
2024-12-09 13:23:35 +0530

    Update ProductCollection.java

--- ecommerceapp/Collection/ProductCollection.java
+++ ecommerceapp/Collection/ProductCollection.java
@@ -1,1 +1,16 @@
+package com.java.ecommerceApp.collection;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jspiders.ecommerceApp.entity.Product;
+
+public class ProductCollection {
+
+	private List<Product> products = new ArrayList<Product>();
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+}
