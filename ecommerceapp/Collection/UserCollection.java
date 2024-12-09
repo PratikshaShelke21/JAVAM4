69a1e995a86f595edc8a321923b51fe49c542ef4
PratikshaShelke21 <shelkepratu2102@gmail.com>
2024-12-09 13:24:50 +0530

    Update UserCollection.java

--- ecommerceapp/Collection/UserCollection.java
+++ ecommerceapp/Collection/UserCollection.java
@@ -1,1 +1,16 @@
+package com.java.ecommerceApp.collection;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jspiders.ecommerceApp.entity.User;
+
+public class UserCollection {
+
+	private List<User> users = new ArrayList<User>();
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+}
